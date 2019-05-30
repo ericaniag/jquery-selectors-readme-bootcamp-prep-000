2ec6bc6378ac8f974e538125472c4a6126b81527
Eric Aniag <ericaniag@gmail.com>
2019-05-30 22:26:54 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -8,13 +8,13 @@ function lastImageSelection() {
 }
 
 function ninjaBabySelector() {
-  
+  $('#baby-ninja')
 }
 
 function divSelector() {
-  
+  $('.pics')
 }
 
 function firstListItem() {
-  
+  $('ul pic-list:first-child')
 }
